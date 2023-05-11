d874aa648073f831794e68376eb0871528b256c3
Setup Game <ayoubmzari33@gmail.com>
2023-05-11 13:20:00 +0100

    second

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_sslify import SSLify
 
 app = Flask(__name__)
-
+sslify = SSLify(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World! This is my domain.'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=443)
+    app.run(host='mzari.me')
